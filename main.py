cb180e5a1f519a02a6c7b03fd487ba8059f9acea
TRAMAZOOL <tramaman003@gmail.com>
2025-10-30 08:45:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI , HTTPException
+import requests 
+
+app = FastAPI()
+
+@app.get('/') 
+def root ():
+    return {'everything is good ! ⭐'}
+
+
+@app.get('/ai')
+def ai_reply(query : str , id) :
+    response = requests.post('https://n8n.hjhj.me/webhook/4293fd14-e226-4730-9663-37d7b10c37cc' 
+                             , json= {'userMessage' : query , 'userID': id})
+    return (response.text)
