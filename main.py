4f3e542763e53c0a85575967ac92231ad7cf15db
TRAMAZOOL <tramaman003@gmail.com>
2025-11-04 06:09:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def root ():
 
 
 @app.get('/ai')
-def ai_reply(query : str , id) :
+def ai_reply(name : str , query : str , id) :
     response = requests.post('https://n8n.hjhj.me/webhook/4293fd14-e226-4730-9663-37d7b10c37cc' 
-                             , json= {'userMessage' : query , 'userID': id})
+                             , json= {'userName' : name ,'userMessage' : query , 'userID': id})
     return (response.text)
